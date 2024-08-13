bb9822cf6e3f3e106428b2c500b9ff501ba9219f
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-08-13 20:41:37 +0200

    Working on app setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode, useState, createContext } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const AuthContext = createContext();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+function Main() {
+  const [authState, setAuthState] = useState(false);
+  return (
+    <>
+      <AuthContext.Provider value={{ authState, setAuthState }}>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthContext.Provider>
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Main />
+  </StrictMode>
+);
