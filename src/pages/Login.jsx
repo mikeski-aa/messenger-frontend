da18e3cf4cfd1257c6a6a813d1e65c52ca86b33f
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-08-14 14:02:36 +0200

    Login page looks OK

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../App";
 import dmLogo from "../assets/DM_ME_LOGO.png";
+import "../styles/login.css";
 
 function Login() {
   const authContext = useContext(AuthContext);
@@ -9,39 +10,54 @@ function Login() {
     console.log("Context false");
   }
 
+  // need to add some more styling to this page, in particular background - transparent, with overlay.
+
   return (
     <>
-      <div className="loginContent">
-        <div className="loginLogoHeader">
-          <img src={dmLogo}></img>
-        </div>
-        <div className="loginMain">
-          <form>
-            <div className="emailLogin">
-              <label htmlFor="email" className="labelEmail">
-                EMAIL
-              </label>
-              <div className="emailInputDiv">
-                <input
-                  type="email"
-                  name="email"
-                  className="emailInputBox"
-                  placeholder="youremail@website.com"
-                />
+      <div className="pageContainer">
+        <div className="loginContent">
+          <div className="loginLogoHeader">
+            <img src={dmLogo} className="logoImg"></img>
+          </div>
+          <div className="loginMain">
+            <div className="welcomeHeadingDiv">
+              <h1 className="welcomeHeading">Welcome back</h1>
+            </div>
+            <form className="loginForm">
+              <div className="emailLogin">
+                <label htmlFor="email" className="labelEmail">
+                  EMAIL
+                </label>
+                <div className="emailInputDiv">
+                  <input
+                    type="email"
+                    name="email"
+                    className="emailInputBox"
+                    placeholder="youremail@website.com"
+                  />
+                </div>
               </div>
-            </div>
-            <div className="passwordLogin">
-              <label htmlFor="password" className="labelPassword">
-                PASSWORD
-              </label>
-              <div className="passwordInputDiv">
-                <input type="password" name="password" />
+              <div className="passwordLogin">
+                <label htmlFor="password" className="labelPassword">
+                  PASSWORD
+                </label>
+                <div className="passwordInputDiv">
+                  <input
+                    type="password"
+                    name="password"
+                    className="passwordInputBox"
+                  />
+                </div>
               </div>
-            </div>
-            <div className="buttonContainer">
-              <button className="submitForm">Login</button>
-            </div>
-          </form>
+              <hr></hr>
+              <div className="buttonContainer">
+                <button className="submitButton">Login</button>
+              </div>
+            </form>
+          </div>
+          <div className="createNew">
+            Don't have an account? <a href="/register">Register!</a>
+          </div>
         </div>
       </div>
     </>
