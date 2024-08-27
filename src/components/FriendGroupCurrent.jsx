4d78d0f67b4b0ceacaefa0a14929624b41f2de0d
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-08-27 12:07:23 +0200

    Working on group pages

---
+++ src/components/FriendGroupCurrent.jsx
@@ -0,0 +1,40 @@
+import "../styles/friendgroupcurrent.css";
+import person from "../assets/person.svg";
+import addCircle from "../assets/addCircle.svg.svg";
+import { useContext, useState } from "react";
+import { AuthContext } from "../App";
+
+function FriendGroupCurrent(props) {
+  const [menuOpen, setMenuOpen] = useState("closed");
+  const authContext = useContext(AuthContext);
+
+  const handleOpenMore = () => {
+    if (menuOpen === "closed") {
+      setMenuOpen("open");
+    } else {
+      setMenuOpen("closed");
+    }
+  };
+
+  return (
+    <>
+      <div className="friendProfileContainer">
+        <div className="friendMain">
+          <img src={person} className={"personImg " + props.status}></img>
+          <div className="friendName">{props.username}</div>
+        </div>
+        <div className="friendBtn">
+          <button className="moreOptBtn">
+            <img
+              src={addCircle}
+              className="addCircleSvg"
+              onClick={handleOpenMore}
+            ></img>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default FriendGroupAdd;
--- src/pages/Groups.jsx
+++ src/pages/Groups.jsx
@@ -1,9 +1,12 @@
 import "../styles/groups.css";
 import { AuthContext } from "../App";
 import { useContext, useEffect, useState } from "react";
+import FriendGroupAdd from "../components/FriendGroupAdd";
+import { v4 as uuidv4 } from "uuid";
 
 function Groups() {
   const authContext = useContext(AuthContext);
+  const [tempGroupFriends, setTempGroupFriends] = useState([]);
 
   const handleCreateGroupClick = () => {};
 
@@ -12,11 +15,28 @@ function Groups() {
       <div className="groupContainer">
         <h1>Groups</h1>
         <div className="mainCont">
-          <div className="createGroundDiv">
+          <div className="createGroupDiv">
             <button className="createGroupBtn" onClick={handleCreateGroupClick}>
               Create group
             </button>
-            <div className="groupFriendsContainer"></div>
+            <div className="tempGroupFriends">
+              <h3>Friends added to group:</h3>
+              <div className="tempFriendsContainer"></div>
+            </div>
+            <div className="groupFriendsAdd">
+              <h3>Add friend to group</h3>
+              <div className="groupFriendsContainer">
+                {authContext.friends.map((friend) => (
+                  <FriendGroupAdd
+                    username={friend.username}
+                    status={friend.status}
+                    id={friend.id}
+                    key={uuidv4()}
+                  />
+                ))}
+              </div>
+            </div>
+            <button className="confirmGroupBtn">Confirm group create</button>
           </div>
           <div className="currentGroups">GROUPS GO HERE</div>
         </div>
