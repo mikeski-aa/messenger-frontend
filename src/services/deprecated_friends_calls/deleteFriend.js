c99bf0283853c1667da86f172fa358538073fec5
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-11 11:22:58 +0200

    Refactor of friend calls done

---
+++ src/services/deprecated_friends_calls/deleteFriend.js
@@ -0,0 +1,30 @@
+import { LOCAL_URL } from "../../../utils/url.const";
+
+async function deleteFriend(userA, userB) {
+  // const url = `http://localhost:3000/api/friends?userA=${userA}&userB=${userB}`;
+
+  const url = LOCAL_URL + `friends?userA=${userA}&userB=${userB}`;
+
+  const headerinfo = {
+    Authorization: "bearer " + localStorage.getItem("token"),
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: headerinfo,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default deleteFriend;
--- src/services/friendCalls.js
+++ src/services/friendCalls.js
@@ -27,4 +27,25 @@ async function deleteFriend(userA, userB) {
   }
 }
 
-export default deleteFriend;
+async function updateUserFriends(idA, idB, reqId) {
+  // const url = `http://localhost:3000/api/friends?userA=${idA}&userB=${idB}&reqId=${reqId}`;
+  const url = LOCAL_URL + `friends?userA=${idA}&userB=${idB}&reqId=${reqId}`;
+
+  const headerinfo = {
+    Authorization: "bearer " + localStorage.getItem("token"),
+  };
+  try {
+    const response = await fetch(url, { method: "PUT", headers: headerinfo });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { deleteFriend, updateUserFriends };
